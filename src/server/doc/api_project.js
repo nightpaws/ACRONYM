2a220c49de6ae14b12f4fedb2971cb4d33376732
= <=>
2015-11-07 23:51:16 +0000

    API documentation

---
+++ src/server/doc/api_project.js
@@ -0,0 +1,26 @@
+define({
+  "name": "CS413 ACRONYM API",
+  "version": "0.0.1-ALPHA",
+  "description": "ACRONYM: A Complex Refrigerator and Organiser for Nutritious and Yummy Munchies",
+  "title": "ACRONYM: A Complex Refrigerator and Organiser for Nutritious and Yummy Munchies",
+  "url": "https://ANIPADDRESS/api",
+  "order": [
+    "GetApp"
+  ],
+  "header": {
+    "title": "Info",
+    "content": "<p>Below is the documentation for our group project for the class CS413 at the University of Strathclyde. The Github repo can be found at <a href=\"https://github.com/nightpaws/Elite-Team-6\">GitHub</a></p>\n"
+  },
+  "footer": {
+    "title": "Copyright",
+    "content": "<p>Copyright 2015</p>\n<p>Thomas Maxwell      - hello@tom-maxwell.com</p>\n<p>Craig Morrison      - craig.morrison.2014@uni.strath.ac.uk</p>\n<p>Grant Toghill       - wsb12157@uni.strath.ac.uk</p>\n<p>Paul Burns          - trb12149@uni.strath.ac.uk</p>\n<p>Thomas Sincliar     -</p>\n<p>This project can not be copied and/or distributed without the express permission of the authors.</p>\n"
+  },
+  "sampleUrl": false,
+  "apidoc": "0.2.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2015-11-07T23:41:02.606Z",
+    "url": "http://apidocjs.com",
+    "version": "0.13.1"
+  }
+});
