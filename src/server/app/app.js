d92cadd60ce0b12a891aae10ef3dd4d02c802fda
= <hello@tom-maxwell.com>
2015-12-18 15:00:18 +0000

    try again

--- src/server/app/app.js
+++ src/server/app/app.js
@@ -45,7 +45,7 @@ var app = function(){
 
 	//serve index
 	/**
-	 * @api {get} / Get website
+	 * @api {get} / Get Application
 	 * @apiName GetApp
 	 * @apiGroup GetApp
 	 *
@@ -58,20 +58,6 @@ var app = function(){
 		res.sendFile('public/index.html');
 	});
 
-	/**
-	 * @api {get} / Get website
-	 * @apiName GetApp
-	 * @apiGroup GetApp
-	 *
-	 * @apiDescription
-	 * Grabs the index html file that is the wonderful SPA.
-	 *
-	 * @apiSuccess html file
-	 */
-	app.get('/dashboard/', function(req,res) {
-		res.sendFile('public/dashboard/index.html');
-	});
-
 
 	/*
 	 * Set up middleware
@@ -79,11 +65,11 @@ var app = function(){
 
 	//auth
 	var auth = require('./middleware/Auth.js');
-	app.use('dashboard/api/', auth);
+	app.use('/api/', auth);
 
 	//check permissions
 	var entitlements = require('./middleware/Entitlements');
-	app.use('dashboard/api/', entitlements);
+	app.use('/api/', entitlements);
 
 	//parse the json we have received
 	app.use(bodyParser.urlencoded({ extended: false }));
@@ -101,7 +87,7 @@ var app = function(){
 	 * Done in external file for sensibleness
 	 */
 	var APIControlRouter = require('./routes/APIControlRouter')();
-	app.use('dashboard/api', APIControlRouter);
+	app.use('/api', APIControlRouter);
 
 	//Got here? well we have no fucking idea what you want! have the index, the app will deal with your 404
     app.use('*', function(req, res, next){
