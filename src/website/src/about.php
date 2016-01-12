7de01fa2b20cc49a1a3d2c06bccde7e0a8633c39
Thomas Maxwell <hello@tom-maxwell.com>
2016-01-12 15:53:40 +0000

    website: about page

--- src/website/src/about.php
+++ src/website/src/about.php
@@ -2,24 +2,39 @@
 <?php include ('toolbar.php'); ?>
 
 <main>
-	<div class="content-primary text-center">
+	<div class="content-primary text-center p-override" >
 
 		<p>This is a project that was created for the class <a href="https://local.cis.strath.ac.uk/wp/teaching/undergraduate/syllabuses/cs413/">CS413</a> at the <a href="https://www.strath.ac.uk/">University of Strathclyde, Glasgow</a>.</p>
 		</br>
-		<p>The aim was to create an embedded gadget of some description. We aimed to capitalize on the current trend in the IoT and build a smart fridge that could be used to monitor the state and contents by users and those that are in positions of care. This means our system is designed to be easy to use with a clean straight forward design.</p> 
+		<p>The aim was to create an embedded gadget of some description, with the specification being left open. Thus we aimed to capitalize on the current trend in the IoT and build a smart fridge, and the supporting infrastructure, that could be used to monitor the state of the device and contents by users and those that are in positions of care. This means our system is designed to be easy to use with a clean straight forward design.</p> 
 		</br>
 		<p>Development Team:</p>
 	</div>
 	<div class="content-secondary no-padding full">
 
 		<div class="person-info half text-center">
-			<h3><a href="https://tom-maxwell.com">Thomas Maxwell</a></h3>
+			<h3>Thomas Maxwell</h3>
 		    <p>Lead Application Server and SPA Client Developer</p>
+		    <div class="person-detials">
+		    	<ul>
+		    		<li><i class="fa fa-link"></i><a href="https://tom-maxwell.com">website</a></li>
+		    		<li><i class="fa fa-github"></i><a href="https://github.com/confuddledpenguin">ConfuddledPenguin</a></li>
+		    		<li><i class="fa fa-linkedin"></i><a href="https://uk.linkedin.com/in/confuddledpenguin">Thomas Maxwell</a></li>
+		    		<li><i class="fa fa-twitter"></i><a href="https://twitter.com/confuddledpengu">ConfuddedPenguin</a></li>
+		    	</ul>
+		    </div>
 		</div>
 
 		<div class="person-info half text-center">
 			<h3><a href="https://nightpaws.eu">Craig Morrison</a></h3>
 		    <p>Lead Server Developer, Git and issue management</p>
+		    <div class="person-detials">
+		    	<ul>
+		    		<li><i class="fa fa-link"></i><a href="https://nightpaws.eu">website</a></li>
+		    		<li><i class="fa fa-github"></i><a href="https://github.com/nightpaws">nightpaws</a></li>
+		    		<li><i class="fa fa-linkedin"></i><a href="https://uk.linkedin.com/in/craigmorrison1">Craig Morrison</a></li>
+		    	</ul>
+		    </div>
 		</div>
 	</div>
 	<div class="clear"></div>
@@ -27,11 +42,25 @@
 		<div class="person-info half text-center">
 			<h3>Grant Toghill</h3>
 		    <p>Lead Web Developer, Project Tracking and Camera Man</p>
+		    <div class="person-detials">
+		    	<ul>
+		    		<li><i class="fa fa-github"></i><a href="https://github.com/ToggleWhenReady">ToggleWhenReady</a></li>
+		    		<li><i class="fa fa-linkedin"></i><a href="ttps://uk.linkedin.com/in/granttoghilln">Grant Toghill</a></li>
+		    		<li><i class="fa fa-twitter"></i><a href="https://twitter.com/ToggleWhenReady">ToggleWhenReady</a></li>
+		    	</ul>
+		    </div>
 		</div>
 
 		<div class="person-info half text-center">
 			<h3>Paul Burns</h3>
 		    <p>Lead Embedded Code Developer</p>
+		    <div class="person-detials">
+		    	<ul>
+		    		<li><i class="fa fa-github"></i><a href="https://github.com/AGreatPigeon">AGreatPigeon</a></li>
+		    		<li><i class="fa fa-linkedin"></i><a href="	
+https://uk.linkedin.com/in/paul-burns-b975a238">Paul Burns</a></li>
+		    	</ul>
+		    </div>
 		</div>
 	</div>
 	<div class="clear"></div>
@@ -39,13 +68,24 @@
 		<div class="person-info half text-center">
 			<h3>Thomas Sinclair</h3>
 		    <p>Hardware Monkey</p>
+		    <div class="person-detials">
+		    	<ul>
+		    		<li><i class="fa fa-github"></i><a href="https://github.com/tasinclair1">tasinclair1</a></li>
+		    		<li><i class="fa fa-linkedin"></i><a href="https://uk.linkedin.com/in/tasinclair1">Thomas Sinclair</a></li>
+		    	</ul>
+		    </div>
 		</div>
 
 		<div class="person-info half text-center">
-			<h3><a href="http://www.strath.ac.uk/staff/smeedduncanmr/">Duncan Smeed</a></h3>
+			<h3>Duncan Smeed</h3>
 		    <p>Head of regional catering<br></p>
 			<p style="font-style: italic;">Previously head of national catering<br>
 		    Previously head of global catering</i></p>
+		    <div class="person-detials">
+		    	<ul>
+		    		<li><i class="fa fa-link"></i><a hred="http://www.strath.ac.uk/staff/smeedduncanmr/">website</a></li>
+		    	</ul>
+		    </div>
 		</div>
 	</div>
 	<div class="clear"></div>
