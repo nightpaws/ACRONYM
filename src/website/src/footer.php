30d876fbcd9ba44350cf4a817b1e208fc386aedb
Thomas Maxwell <hello@tom-maxwell.com>
2016-01-12 15:13:38 +0000

    mostly finished homepage for website, is responsive

--- src/website/src/footer.php
+++ src/website/src/footer.php
@@ -1,22 +1,25 @@
 </main>
 <footer>
-	<div id="copyright" class="">
-		<ul>
-			<li>&copy</li>
-			<li><a href="https://tom-maxwell.com">Thomas Maxwell</a></li>
-			<li><a href="https://nightpaws.eu">Craig Morrison</a></li>
-			<li>Grant Toghill</li>
-			<li>Paul Burns</li>
-			<li>Thomas Sinclair</li>
-		</ul>
-	</div>
 	<div id="disclaimer" class="">
 		<p>
 			Made for <a href="https://local.cis.strath.ac.uk/wp/teaching/undergraduate/syllabuses/cs413/">CS413</a>, <a href="https://www.strath.ac.uk/"> Strathclyde University</a>
 			<a href="https://github.com/nightpaws/Elite-Team-6"><i class="fa fa-github"></i> Github</a>
 		</p>
 	</div>
+	<div id="copyright" class="">
+		<ul>
+			<li>Lead Authors:</li>
+			<li><a href="https://tom-maxwell.com">Thomas Maxwell</a></li>
+			<li><a href="https://nightpaws.eu">Craig Morrison</a></li>
+			<li>Grant Toghill</li>
+			<li>Paul Burns</li>
+			<li>Thomas Sinclair</li>
+			<div class="clearfix"></div>
+		</ul>
+	</div>
+	<div class="clearfix"></div>
 </footer>
-
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.6/js/bootstrap.min.js"></script>
 </body>
 </html>
--- src/website/src/index.php
+++ src/website/src/index.php
@@ -2,27 +2,19 @@
 <?php include ('toolbar.php'); ?>
 
 <main>
-	<div class="content-primary text-center">
+	<div class="content-primary text-center" id="launch-container">
+		<a href="dashboard/" id="launch">Launch Application</a>
+		
+	</div>
+	<div class="content-secondary text-center" >
 		<h1>What is ACRONYM?</h1>
-		<h4>Why should I want one?</h4>
 	</div>
-	<div class="content-secondary text-center">
-		<p>A Complex Refrigerator and Organiser for Nutritious and Yummy Munchies (ACRONYM) is at the leading edge of the Raspberry Pi and Arduino smart fridge market. 
-			Have you ever thought about having a way of being able to keep track of what is in your fridge while on the move or even just to look at what you actually
-			 have while in for your weekly shop then ACRONYM is the solution for you.</p>
-	</div>
-	<div class="content-primary text-center">
-		<p>For our original concept of making a fridge contents and status being made available easily on the go we have developed a system using both the abilities
-		 and cost efficiency of both the Raspberry Pi and Arduino to implement a design that would be easy to replicate along with having the ability to be expanded
-		  in the future with new features.</p>
-	</div>
-	<div class="content-secondary text-center">
-		<p>Currently any users in the system can see the contents of any of their fridges on the system, temperature of the fridge, door status, and weight of the fridge.</p>
+	<div class="content-primary">
+		<p>A Complex Refrigerator and Organiser for Nutritious and Yummy Munchies (ACRONYM) is at the leading edge of the Raspberry Pi and Arduino smart fridge market. Have you ever thought about having a way of being able to keep track of what is in your fridge while on the move or even just to look at what you actually have while in for your weekly shop then ACRONYM is the solution for you.</p>
+		<p>For our original concept of making a fridge contents and status being made available easily on the go we have developed a system using both the abilities and cost efficiency of both the Raspberry Pi and Arduino to implement a design that would be easy to replicate along with having the ability to be expanded in the future with new features.</p>
+		<p>By using our straightforward web based interface, anyone using our systems can browse the contents of any of their fridges through the system, check the temperature of their fridges, whether they’ve left the door open, and the weight of their fridge contents to help them gauge how full it is. This can be checked from any modern web enabled device through our secure and convenient portal.</p>
 
 		<!-- <p>ACRONYM is an project for an Embedded Systems class at the University of Strathclyde Scotland. We have aimed to create a fridge system that will enable users, and carers  to monitor the status and contents of the fridge</p> -->
-	</div>
-	<div class="content-primary" id="launch-container">
-		<a href="dashboard/" id="launch">Launch Application</a>
 	</div>
 
 </main>
